82b90db777116fe9345208678d69047fd96ebde0
sebastianbreit <sebastianbreit311997@gmail.com>
2021-12-19 16:01:28 +0100

    Exercise 1 - final submission

--- exercise1/ex01_task1/main.py
+++ exercise1/ex01_task1/main.py
@@ -1,34 +1,34 @@
 import csv
 import spacy
 import logging
-logging.basicConfig( level=logging.DEBUG)
-
+logging.basicConfig(level=logging.DEBUG)
 
 
 def posTagging(doc):
-    out_pos=[]
+    # Regarding part 2 of the bonus exercise, I did not understand what is
+    # meant with start and end indices of a token
+    out_pos = []
 
-    header = ["Text", "POS Tag"]
+    header = ["Text", "POS Tag", "Named Entity Label"]
     out_pos.append(header)
 
     for sent in doc.sents:
         for token in sent:
-            out_pos.append([token.text, token.pos_])
+            out_pos.append([token.text, token.pos_, token.ent_type_])
         # After sentence append newline
-        out_pos.append(["",""])
+        out_pos.append(["", "", ""])
     return out_pos
 
+
 def namedEntities(doc):
-    # 1 text
-    # 2 start index
-    # 3 end index
-    # 4 label
-    out_entities=[]
-    header = ["Text","Start Index","End Index","Label"]
+    out_entities = []
+    header = ["Text", "Start Index", "End Index", "Label"]
     out_entities.append(header)
     for ent in doc.ents:
-        out_entities.append([ent.text, ent.start_char, ent.end_char, ent.label_])
+        out_entities.append(
+            [ent.text, ent.start_char, ent.end_char, ent.label_])
     return out_entities
+
 
 def write_to_file(file, content):
     with open(file, 'w') as f:
@@ -47,8 +47,7 @@ def main():
     doc = nlp(example_string)
 
     write_to_file('pos_example.csv', posTagging(doc))
-    write_to_file('entities_example.csv',namedEntities(doc))
-
+    write_to_file('entities_example.csv', namedEntities(doc))
 
 
 if __name__ == "__main__":
