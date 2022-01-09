365ddcab8cf84a0b37a15d4a66e6e9443520ce9a
sebastianbreit <sebastianbreit311997@gmail.com>
2022-01-09 18:48:04 +0100

    ex2: task 2 - basic impl

--- exercise2/Vue/main.js
+++ exercise2/Vue/main.js
@@ -1,12 +1,28 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        input:'Default test text',
+        highlight:'te',
+        color:'',
+        tokensHL:['Default', 'test', 'text']
     },
     methods: {
+        createTokens: function (){
+            tokens= this.input.split(" ")
+            this.tokensHL=[]
+            for (key in tokens){
+                this.tokensHL.push(tokens[key].replaceAll(this.highlight, "<font color='"+this.color+"'>"+this.highlight+"</font>"))
+            }
 
+        }
     },
     computed: {
+        markHL: function (){
+            return this.input.replaceAll(this.highlight, "<mark>"+this.highlight+"</mark>")
+        },
+        classHL: function (){
+            return this.input.replaceAll(this.highlight, "<span class='highlight'>"+this.highlight+"</span>")
+        }
 
     }
 })
